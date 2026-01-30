1cdc2738bebd9b292a0d04fb11a9080a85d8b2b3
sawantriddhi-hash <sawantriddhi@eng.rizvi.edu.in>
2026-01-30 23:25:49 +0530

    Implement factorial calculation in main.py
    Fix typo in print statement and implement factorial calculation.

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ print("--- Factorial Finder ---\n")
 
 
 # Write your code here
+print("---Fcatorial Finder----\n")  
+N = int(input("Enter Number:"))
+fact = 1
+for N in range(N , 0 , -1):
+    fact = fact*N
+if(N<0):
+    print(f"Factorial of {abs(N)} is Not Defined")
+else:
+    print(f"Factorial of {N} is {fact}")    
+
+
